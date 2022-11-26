cf4c2087f491a3505f85fd77a03425fdd94887e4
skydreamer21 <skydreamer210@gmail.com>
2022-11-26 13:37:46 +0900

    feat(GameServcie): 랜덤 정답 생성 기능 구현

--- src/main/java/baseball/service/GameService.java
+++ src/main/java/baseball/service/GameService.java
@@ -1,5 +1,15 @@
 package baseball.service;
 
+import baseball.domain.AnswerNumbers;
+import baseball.domain.BaseballNumbers;
+import baseball.domain.NumbersProperties;
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class GameService {
     private static GameService instance = new GameService();
 
@@ -9,7 +19,22 @@ public class GameService {
         return instance;
     }
 
+    public AnswerNumbers createAnswer() {
+        return new AnswerNumbers(generateUniqueNumbers());
     }
 
+    private List<Integer> generateUniqueNumbers() {
+        Set<Integer> numberSet = new HashSet<>();
+        while (numberSet.size() < NumbersProperties.SIZE.get()) {
+            numberSet.add(generateNumber());
+        }
+        return new ArrayList<>(numberSet);
+    }
 
+    private int generateNumber() {
+        return Randoms.pickNumberInRange(
+                NumbersProperties.MIN_NUMBER.get(),
+                NumbersProperties.MAX_NUMBER.get()
+        );
+    }
 }
