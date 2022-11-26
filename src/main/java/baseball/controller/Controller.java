9e97e90e264f2e8eae6dd3b4aafec5a1ec676422
khyojun <nandong5923@naver.com>
2022-11-26 12:24:21 +0900

    feat(baseball): 컨트롤러에서 게임을 시작하는 곳과 연결, 게임 시작 시 출력 기능 추가

---
+++ src/main/java/baseball/controller/Controller.java
@@ -0,0 +1,21 @@
+package baseball.controller;
+
+import baseball.domain.Game;
+import baseball.view.InputView;
+import baseball.view.OutputView;
+
+public class Controller {
+
+    InputView inputView;
+    OutputView outputView;
+    public Controller(InputView inputView, OutputView outputView) {
+        this.inputView=inputView;
+        this.outputView=outputView;
+    }
+
+    public void run() {
+        outputView.printStart();
+        Game.start();
+
+    }
+}
---
+++ src/main/java/baseball/domain/Game.java
@@ -0,0 +1,9 @@
+package baseball.domain;
+
+public class Game {
+
+    public static void start() {
+
+
+    }
+}
