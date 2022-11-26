ca8acdb83a10480bc8585afacf6ae760e662a3af
skydreamer21 <skydreamer210@gmail.com>
2022-11-26 12:21:29 +0900

    feat(ErrorMessage): 필요 에러메세지 생성

---
+++ src/main/java/baseball/constant/ErrorMessage.java
@@ -0,0 +1,20 @@
+package baseball.constant;
+
+import baseball.domain.NumbersProperties;
+
+public enum ErrorMessage {
+    INVALID_SIZE("숫자의 개수가 " + NumbersProperties.SIZE.get() + "이어야 합니다."),
+    INVALID_RANGE(NumbersProperties.MIN_NUMBER.get() + "부터 " +
+            NumbersProperties.MAX_NUMBER.get() + "까지의 숫자만 허용됩니다.");
+
+    private static final String error = "[ERROR] ";
+    private final String message;
+
+    private ErrorMessage (String message) {
+        this.message = message;
+    }
+
+    public String get() {
+        return error + message;
+    }
+}
