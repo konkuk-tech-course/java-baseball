df2cb17a06a190bc6b9df456c799b8a6a87dc4e9
skydreamer21 <skydreamer210@gmail.com>
2022-11-26 12:59:04 +0900

    feat(GameServcie): GameServcie 싱글톤 틀 구현

---
+++ src/main/java/baseball/service/GameService.java
@@ -0,0 +1,13 @@
+package baseball.service;
+
+public class GameService {
+    private GameService instance = new GameService();
+
+    private GameService() {}
+
+    public static GameService getInstance() {
+        return getInstance();
+    }
+
+
+}
