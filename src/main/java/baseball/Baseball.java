ae1e4b115f47dca1542929def2ce589cf0e34bd6
saehim <saeilee.vxv@gmail.com>
2022-11-26 12:20:50 +0900

    feat (Baseball) : 중복 숫자 유효성 체크 메서드

--- src/main/java/baseball/Baseball.java
+++ src/main/java/baseball/Baseball.java
@@ -1,4 +1,11 @@
 package baseball;public class Baseball {
+    private void checkDuplication(List<Integer> numbers) {
+        Set<Integer> numbersSet = new HashSet<>(numbers);
+        if (numbersSet.size() != numbers.size()) {
+            throw new IllegalArgumentException("[ERROR] 각 숫자는 1이상 9이하로 입력해주세요.");
+        }
+    }
+
     private void checkValidRange(List<Integer> numbers) {
         for (int num : numbers) {
             if (1 > num || num > 9) {
