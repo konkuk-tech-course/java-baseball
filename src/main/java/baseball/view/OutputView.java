b857eb8d9fdeb7ee0705fc5fbb0f47e502c3b711
skydreamer21 <skydreamer210@gmail.com>
2022-11-26 15:06:57 +0900

    feat(OutputView): 싱글톤 틀 구현

---
+++ src/main/java/baseball/view/OutputView.java
@@ -0,0 +1,12 @@
+package baseball.view;
+
+public class OutputView {
+    private static OutputView instance = new OutputView();
+
+    private OutputView() {}
+
+    public static OutputView getInstance() {
+        return instance;
+    }
+
+}
