4d4f8f3d5eb637a56bae12a8747189b02cd89a55
saehim <saeilee.vxv@gmail.com>
2022-11-26 16:07:23 +0900

    feat (controller) : 라운드 구현
    - 숫자입력
    - 판정
    - 출력

--- src/main/java/baseball/controller/BaseballController.java
+++ src/main/java/baseball/controller/BaseballController.java
@@ -29,6 +29,28 @@ public class BaseballController {
         return false;
     }
 
+    private void endGame() {
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+        retry();
+    }
+
+    private boolean retry() {
+        int command = userInput.askRetry();
+        if (command==1) {
+            return startGame();
+        }
+        return false;
+    }
+
+    private void playRound() {
+        Baseball userBaseball = userInput.askInputBaseball();
+        JudgementType judgement = baseballJudge.judgement(userBaseball);
+        System.out.println(judgement.toString());
+        endFlag = baseballJudge.checkAnswer(judgement);
+    }
+
+
+
     private void initNewGame() {
         System.out.println("숫자 야구 게임을 시작합니다.");
         endFlag = false;
