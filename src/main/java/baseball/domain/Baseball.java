c41e57a590c2c9e739f0c332a43a38c222f0e214
saehim <saeilee.vxv@gmail.com>
2022-11-26 14:28:10 +0900

    fix (domain) : 예외처리시 오류메시지 오타

--- src/main/java/baseball/domain/Baseball.java
+++ src/main/java/baseball/domain/Baseball.java
@@ -1,5 +1,7 @@
 package baseball.domain;
 
+import baseball.view.Error;
+
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
@@ -9,12 +11,19 @@ public class Baseball {
     private List<Integer> numbers;
     private final int NUMBERS_SIZE = 3;
 
+    public Baseball(List<Integer> numbers) {
+        checkSize(numbers);
+        checkValidRange(numbers);
+        checkDuplication(numbers);
+        this.numbers = numbers;
+    }
+
     public Baseball(int num1, int num2, int num3) {
         List<Integer> numbers = Arrays.asList(num1, num2, num3);
         checkSize(numbers);
         checkValidRange(numbers);
         checkDuplication(numbers);
-        this.numbers = Arrays.asList(num1, num2, num3);
+        this.numbers = numbers;
     }
 
     public List<Integer> getNumbers() {
@@ -23,21 +32,21 @@ public class Baseball {
 
     private void checkSize(List<Integer> numbers) {
         if (numbers.size() != NUMBERS_SIZE) {
-            throw new IllegalArgumentException("[ERROR] 3자리 숫자를 입력해주세요.");
+            throw new IllegalArgumentException("3자리 숫자를 입력해주세요.\n");
         }
     }
 
     private void checkDuplication(List<Integer> numbers) {
         Set<Integer> numbersSet = new HashSet<>(numbers);
         if (numbersSet.size() != numbers.size()) {
-            throw new IllegalArgumentException("[ERROR] 각 숫자는 1이상 9이하로 입력해주세요.");
+            throw new IllegalArgumentException("각 숫자는 1이상 9이하로 입력해주세요.\n");
         }
     }
 
     private void checkValidRange(List<Integer> numbers) {
         for (int num : numbers) {
             if (1 > num || num > 9) {
-                throw new IllegalArgumentException("[ERROR] 각 숫자는 1이상 9이하로 입력해주세요.");
+                throw new IllegalArgumentException("각 숫자는 1이상 9이하로 입력해주세요.\n");
             }
         }
     }
