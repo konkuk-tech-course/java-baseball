b2ab42f88d573cd86a747fc8c24c34add72122e4
saehim <saeilee.vxv@gmail.com>
2022-11-26 12:21:29 +0900

    feat (Baseball) : baseball 클래스 생성자

--- src/main/java/baseball/Baseball.java
+++ src/main/java/baseball/Baseball.java
@@ -1,4 +1,20 @@
-package baseball;public class Baseball {
+package baseball;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class Baseball {
+    List<Integer> numbers;
+
+    public Baseball(int num1, int num2, int num3) {
+        List<Integer> numbers = Arrays.asList(num1, num2, num3);
+        checkValidRange(numbers);
+        checkDuplication(numbers);
+        this.numbers = Arrays.asList(num1, num2, num3);
+    }
+
     private void checkDuplication(List<Integer> numbers) {
         Set<Integer> numbersSet = new HashSet<>(numbers);
         if (numbersSet.size() != numbers.size()) {
@@ -13,4 +29,7 @@ package baseball;public class Baseball {
             }
         }
     }
+
+
+
 }
