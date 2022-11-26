9b15e15546b3941fb133b91f68652bb3cbc40330
skydreamer21 <skydreamer210@gmail.com>
2022-11-26 13:06:23 +0900

    feat(AnswerNumbers): 정답 번호 클래스 생성
    [세부사항]
    - BaseballNumbers 클래스 상속

---
+++ src/main/java/baseball/domain/AnswerNumbers.java
@@ -0,0 +1,9 @@
+package baseball.domain;
+
+import java.util.List;
+
+public class AnswerNumbers extends BaseballNumbers {
+    public AnswerNumbers(List<Integer> input) {
+        super(input);
+    }
+}
