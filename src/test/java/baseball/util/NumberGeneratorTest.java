397b29f4ee9686eafa2e90aa0d85fd30acaf9088
saehim <saeilee.vxv@gmail.com>
2022-11-26 13:15:57 +0900

    test (NumberGenerator) : 3자리 랜덤 숫자 뽑기 테스트

--- src/test/java/baseball/util/NumberGeneratorTest.java
+++ src/test/java/baseball/util/NumberGeneratorTest.java
@@ -1,4 +1,19 @@
+package baseball.util;
+
+import baseball.domain.Baseball;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+
 class NumberGeneratorTest {
-  
+
+    @Test
+    void numberGenerate테스트() {
+        NumberGenerator numberGenerator = new NumberGenerator();
+        Baseball baseball = numberGenerator.numberGenerate();
+        System.out.printf(baseball.getNumbers().toString());
+
+        assertThat(baseball.getNumbers()).hasSize(3);
+    }
 }
