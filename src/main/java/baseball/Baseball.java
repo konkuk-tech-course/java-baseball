1f6b360ac45a0982be6a4f358a4f64b577b31606
saehim <saeilee.vxv@gmail.com>
2022-11-26 12:20:30 +0900

    feat (Baseball) : 숫자 범위 유효성 체크 메서드

--- src/main/java/baseball/Baseball.java
+++ src/main/java/baseball/Baseball.java
@@ -1,2 +1,9 @@
 package baseball;public class Baseball {
+    private void checkValidRange(List<Integer> numbers) {
+        for (int num : numbers) {
+            if (1 > num || num > 9) {
+                throw new IllegalArgumentException("[ERROR] 각 숫자는 1이상 9이하로 입력해주세요.");
+            }
+        }
+    }
 }
