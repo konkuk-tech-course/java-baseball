56495d8987a83609bb3d3c886d2b32f6b673fb12
juno-junho <ssmm0205@naver.com>
2022-12-14 19:35:03 +0900

    feat(InputView): 서로다른 세 수 읽는 기능 구현

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -1,4 +1,27 @@
 package view;
 
+import camp.nextstep.edu.missionutils.Console;
+import util.Util;
+import util.Validator;
+
 public class InputView {
+
+    private final Validator validator;
+
+    public InputView(Validator validator) {
+        this.validator = validator;
+    }
+
+    public void readThreeDifferentNumber() {
+        System.out.print("숫자를 입력해주세요 : ");
+        int number = Util.convertStringToNumber(Console.readLine());
+        validator.validateNumber(number);
+    }
+
+    public static void main(String[] args) {
+        Validator validator = new Validator();
+        InputView inputView = new InputView(validator);
+        inputView.readThreeDifferentNumber();
+    }
+
 }
