284a1fc095640f530fc830e4eb0530f2a0a7d51f
juno-junho <ssmm0205@naver.com>
2022-12-14 21:10:12 +0900

    feat(controller): view와 domain을 통한 구현 완료

--- src/main/java/controller/BaseBallGameController.java
+++ src/main/java/controller/BaseBallGameController.java
@@ -1,7 +1,43 @@
 package controller;
 
-public class BaseBallGameController {
-    public void playTheGame() {
+import constants.Status;
+import domain.Computer;
+import util.Validator;
+import view.InputView;
+import view.OutputView;
 
+import java.util.Map;
+
+import static constants.Code.RESTART;
+
+public class BaseBallGameController {
+
+    public static final int MAX_STRIKE = 3;
+    private final InputView inputView;
+    private final OutputView outputView;
+
+    public BaseBallGameController() {
+        Validator validator = new Validator();
+        this.inputView = new InputView(validator);
+        this.outputView = new OutputView();
+    }
+
+    public void playTheGame() {
+        outputView.printStartMessage();
+        do {
+            Computer computer = new Computer();
+            matchedResultWithComputer(computer);
+        } while (inputView.readStartOrEndCode() == RESTART.getCode());
+    }
+
+    private void matchedResultWithComputer(Computer computer) {
+        while (true) {
+            int numbers = inputView.readThreeDifferentNumber();
+            Map<Status, Integer> resultMap = computer.generateResultMessage(numbers);
+            outputView.printGameMessage(resultMap);
+            if (resultMap.get(Status.STRIKE) == MAX_STRIKE) {
+                break;
+            }
+        }
     }
 }
