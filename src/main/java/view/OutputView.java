78756e0b992766e31d8dcbcab96ba53cc6098198
juno-junho <ssmm0205@naver.com>
2022-12-14 21:06:18 +0900

    feat(OutputView): 게임 결과 메세지를 출력하는 메서드 생성

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -15,4 +15,48 @@ public class OutputView {
     public void printStartMessage() {
         System.out.println("숫자 야구 게임을 시작합니다.");
     }
+
+    public void printGameMessage(Map<Status, Integer> resultMap) {
+        ballCount = resultMap.getOrDefault(BALL, 0);
+        strikeCount = resultMap.getOrDefault(STRIKE, 0);
+        whenNoBallNoStrike();
+        whenNoStrike();
+        whenBallAndStrikeExists();
+        whenNoBall();
+    }
+
+    private void whenNoBall() {
+        if (ballCount == 0 && strikeCount > 0) {
+            System.out.println(strikeCount +"스트라이크");
+            whenAllMatched();
+        }
+    }
+
+    private void whenAllMatched() {
+        if (strikeCount == MAX_STRIKE) {
+            printEndMessage();
+        }
+    }
+
+    private void whenBallAndStrikeExists() {
+        if (ballCount > 0 && strikeCount > 0) {
+            System.out.println(ballCount + "볼 " + strikeCount + "스트라이크");
+        }
+    }
+
+    private void whenNoStrike() {
+        if (ballCount > 0 && strikeCount == 0) {
+            System.out.println(ballCount+"볼");
+        }
+    }
+
+    private void whenNoBallNoStrike() {
+        if (ballCount == 0 && strikeCount == 0) {
+            System.out.println("낫싱");
+        }
+    }
+
+    private void printEndMessage() {
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+    }
 }
