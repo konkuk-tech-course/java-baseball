9e6275c4dd845b500cdf562de374c0095e20e530
juno-junho <ssmm0205@naver.com>
2022-12-14 21:05:08 +0900

    feat(util): number를 list로 변환하는 메서드 생성

--- src/main/java/util/Util.java
+++ src/main/java/util/Util.java
@@ -1,5 +1,8 @@
 package util;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Util {
     public static int convertStringToNumber(String numStr) {
         try {
@@ -7,5 +10,11 @@ public class Util {
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException("[ERROR] 서로다른 세 숫자를 입력하셔야 합니다.");
         }
+    }
+    public static List<Integer> convertNumberToList(int number) {
+        return String.valueOf(number)
+                .chars()
+                .mapToObj(Character::getNumericValue)
+                .collect(Collectors.toList());
     }
 }
