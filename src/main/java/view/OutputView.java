50dba0b01909fc0d28f206674fb7b4cc5f045148
juno-junho <ssmm0205@naver.com>
2022-12-14 21:05:48 +0900

    feat(OutputView): 게임 시작 메세지를 출력하는 메서드 구현

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -1,4 +1,18 @@
 package view;
 
+import constants.Status;
+
+import java.util.Map;
+
+import static constants.Status.BALL;
+import static constants.Status.STRIKE;
+
 public class OutputView {
+
+    public static final int MAX_STRIKE = 3;
+    private int ballCount;
+    private int strikeCount;
+    public void printStartMessage() {
+        System.out.println("숫자 야구 게임을 시작합니다.");
+    }
 }
