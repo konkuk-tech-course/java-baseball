81962d985b5a885eb4a5d96390e8f805cbb03a79
juno-junho <ssmm0205@naver.com>
2022-12-14 19:35:50 +0900

    feat(Validator): 입력 받은 숫자를 검증하는 기능 구현

---
+++ src/main/java/util/Validator.java
@@ -0,0 +1,35 @@
+package util;
+
+public class Validator {
+
+    public static final int INPUT_SIZE = 3;
+
+    public void validateNumber(int number) {
+        validateNumberLength(number);
+        validateDuplicateNumber(number);
+    }
+    private void validateNumberLength(int number) {
+        int numberLength = getNumberLength(number);
+        if (numberLength != INPUT_SIZE) {
+            throw new IllegalArgumentException("[ERROR] 서로 다른 세 자리 수를 입력해 주세요");
+        }
+    }
+
+    private int getNumberLength(int number) {
+        return (int) (Math.log10(number) + 1);
+    }
+
+    private void validateDuplicateNumber(int number) {
+        if (hasDuplicateNum(number)) {
+            throw new IllegalArgumentException("[ERROR] 중복된 문자를 입력하셨습니다");
+        }
+    }
+
+    private boolean hasDuplicateNum(int number) {
+        return getDistinctDuplicateNum(number) != getNumberLength(number);
+    }
+
+    private int getDistinctDuplicateNum(int number) {
+        return (int) String.valueOf(number).chars().distinct().count();
+    }
+}
